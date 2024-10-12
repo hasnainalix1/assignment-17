ed990213275275ac35f197ff17dc3c582ae7c773
hasnian1122 <soomronain22@gmail.com>
2024-10-12 10:14:18 -0700

    url

---
+++ script.js
@@ -0,0 +1,39 @@
+// assignment 1
+// var citytocheck = "karachi";
+// var firstchar = citytocheck.slice(0, 3);
+// var secchar = citytocheck.charAt(3);
+// var otherchar = citytocheck.slice(4);
+// firstchar = firstchar.toLowerCase();
+// secchar = secchar.toUpperCase();
+// otherchar = otherchar.toLowerCase();
+// var cappedCity = firstchar + secchar   + otherchar;
+// console.log(cappedCity);
+
+
+// assignment 2
+// var month = prompt("Enter a month");
+// var charsInMonth = month.length;
+// if (charsInMonth > 5) {
+// monthAbbrev = month.slice(0, 5);
+// console.log(monthAbbrev);
+//  }
+
+
+// assignment 3 
+// // let str = "JavaScript Programming";
+
+// // // Measuring string length
+// // let length = str.length;
+// // console.log(length);  // Output: 23
+
+
+// assignment 4
+// // // Extracting parts of the string using slice
+// // let part = str.slice(0, 10);  // Extracts from index 0 to 9 (10 characters)
+// // console.log(part);  // Output: "JavaScript"
+
+
+// Assignment 15
+// // // Extracting using substring
+// // let part2 = str.substring(4, 14);  // Extracts from index 4 to 13
+// // console.log(part2);  // Output: "Script Pro"
